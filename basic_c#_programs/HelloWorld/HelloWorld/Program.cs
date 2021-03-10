bb458d3e7f5e2b9124a8b2bfdaf4298bc5c1850b
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-10 16:36:32 -0700

    Complete Accounting Welcome Screen console assignment

---
+++ basic_c#_programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+        Console.WriteLine("\"It's time to use an escape sequence!\" \nSaid Johnny and his Mom!");
+        Console.ReadLine();
+    }
+}
---
+++ basic_c#_programs/acme_accounting/acme_accounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace acme_accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
