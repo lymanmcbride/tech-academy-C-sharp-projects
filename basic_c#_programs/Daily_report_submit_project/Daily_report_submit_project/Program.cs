8995b17bb6d9e44b6bf2805777ed9dfe987f80eb
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-11 07:25:53 -0700

    Complete Daily Report Assignment

---
+++ basic_c#_programs/Daily_report_submit_project/Daily_report_submit_project/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily_report_submit_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intro Text
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+
+            //Questions
+            Console.WriteLine("What is your Name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string page_string = Console.ReadLine();
+            Int32 page_number = Convert.ToInt32(page_string); //Converts to int
+
+            Console.WriteLine("Do you need help with anything? (True/False)");
+            string help_string = Console.ReadLine();
+            bool help = Convert.ToBoolean(help_string); //converts to boolean
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positive_experiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many Hours did you study today?");
+            string hours_string = Console.ReadLine();
+            Int16 hours = Convert.ToInt16(hours_string); //converts to int
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. \nHave a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ basic_c#_programs/VariablesAndDatatypes/VariablesAndDatatypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDatatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your Favorite Number, plus 5, is " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char makeX = 'X';
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m; //used mostly for financial transactions, because decimal was made for that
+            double heightInCm = 211.2346029848;
+            float secondsLeft = 2.62f;
+
+            short tempOnMars = -341;
+
+            string myName = "Lyman";
+
+            //casting
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
