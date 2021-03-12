c08b94fa5a0cea37e2a8dd9f63344171e29a9d67
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-11 20:55:49 -0700

    Finish Car Insurance Approval console app to practice boolean logic

---
+++ basic_c#_programs/Car Insurance Approval/Car Insurance Approval/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get info
+            Console.WriteLine("What is your age?");
+            string age_string = Console.ReadLine();
+            Int16 age = Convert.ToInt16(age_string); //convert each bit of info to the corect data type
+
+            Console.WriteLine("Have you ever had a DUI?(True/False)");
+            string dui_string = Console.ReadLine();
+            bool dui = Convert.ToBoolean(dui_string);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets_string = Console.ReadLine();
+            Int16 tickets = Convert.ToInt16(tickets_string);
+
+            //A simple way to display true or false depending on whether each of the conditions are true
+            bool qualified = (age > 15) && (!dui) && (tickets <= 3);
+
+            //Display Result
+            Console.WriteLine("Let's see if you are qualified...\n" + qualified);
+            Console.ReadLine();
+
+
+        }
+    }
+}
