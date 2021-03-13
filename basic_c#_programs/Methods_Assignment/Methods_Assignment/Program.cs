1a43643adf79a3f1eaaacda1f07d3b0919472e06
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-13 16:32:39 -0700

    Finish methods Assignment

---
+++ basic_c#_programs/Methods_Assignment/Methods_Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get input from the user
+            Console.WriteLine("What integer do you want to do math with?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //use the methods. I guess these methods cannot be accessed by an 
+            //instance of the class, you just reference the class to access them
+            int square = common_math.Squared(input);
+            int doubled = common_math.doubled(input);
+            double half = common_math.half(input);
+
+            //print it out
+            Console.WriteLine(
+                "Squared: " + square + 
+                "\nDoubled: "+ doubled + 
+                "\nHalved: " + half
+                );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ basic_c#_programs/Methods_Assignment/Methods_Assignment/common_math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    class common_math //class name
+    {
+        //here are the three methods
+        public static int Squared(int num)
+        {
+            int result = num * num;
+            return result;
+        }
+
+        public static int doubled(int num)
+        {
+            int result = num + num;
+            return result;
+        }
+
+        public static double half(int num)
+        {
+            double result = num * .5; //Whenver dividing, I always make it a floating point number for accuracy
+            return result;
+        }
+    }
+}
