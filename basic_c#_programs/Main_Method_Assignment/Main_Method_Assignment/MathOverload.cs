188296ff666a170009b8da0f004869e48ed2c6bb
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-15 08:13:44 -0600

    Finish Main Method Assignment

---
+++ basic_c#_programs/Main_Method_Assignment/Main_Method_Assignment/MathOverload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class MathOverload
+    {
+        //first method takes int, returns int to power of 3
+        public int PowerThree(int num)
+        {
+            return num * num * num;
+        }
+
+        //second method takes double, returns double, to power of 3, divided by 2
+        public double PowerThree(double num)
+        {
+            return (num * num * num) / 2;
+        }
+
+        //Third method takes string, converts to int, takes it to the power of 3 and adds two.
+        //No division so that decimals won't appear
+        public int PowerThree(string numString)
+        {
+            int num = Convert.ToInt32(numString);
+            return (num * num * num)+2;
+        }
+    }
+}
---
+++ basic_c#_programs/Main_Method_Assignment/Main_Method_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            MathOverload doingMath = new MathOverload();
+
+            //Three method uses
+            int powerOfThree = doingMath.PowerThree(3);
+            double decimalMath = doingMath.PowerThree(3.46);
+            int stringConvert = doingMath.PowerThree("5");
+
+            //print them out
+            Console.WriteLine("Three To the Power of Three: " + powerOfThree);
+            Console.WriteLine("3.46 to the power of 3 divided by two: " + decimalMath);
+            Console.WriteLine("\"5\" converted to a string and given the power of 3 plus two:" + stringConvert);
+            Console.ReadLine();
+        }
+    }
+}
