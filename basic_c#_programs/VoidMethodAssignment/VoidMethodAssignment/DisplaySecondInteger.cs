bb1dd051a63701f1e1a41ee4d910ed6a20f4ce8e
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-15 17:31:38 -0600

    Finish Assignment with a void method

---
+++ basic_c#_programs/VoidMethodAssignment/VoidMethodAssignment/DisplaySecondInteger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class DisplaySecondInteger
+    {
+        public void DisplaySecond(int num1, int num2) //void means that it doesn't return anything
+        {
+            num1 += 1; //Math done with num1
+            Console.WriteLine(num2); //Display num2 to console
+        }
+    }
+}
---
+++ basic_c#_programs/VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            DisplaySecondInteger displayInt = new DisplaySecondInteger();
+
+            //call the method twice
+            displayInt.DisplaySecond(2, 4); //parameters passed in with nums only
+            displayInt.DisplaySecond(num1: 3, num2: 5); //parameters passed in with names assigned for readibility
+
+            //make sure we can see it
+            Console.ReadLine();
+        }
+    }
+}
