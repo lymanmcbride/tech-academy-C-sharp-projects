a4e0f271b526e1b1ebb2f64ffb4feb9112a3a6d9
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-24 07:31:20 -0600

    Finish Structs Assignment

---
+++ basic_c#_programs/Structs/Structs/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    public struct Number //change class to struct. Structs are a value type.
+    {
+        public decimal Amount { get; set; } //our property. it's just a decimal number.
+    }
+}
---
+++ basic_c#_programs/Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number() { Amount = 104.235M };//assigning a decimal amount to a variable. remember that
+                                                            //you have to add M to the end for it to be a decimal.
+            Console.WriteLine($"The number assigned to our struct Number is: {num.Amount}"); //print it out
+            Console.ReadLine();
+        }
+    }
+}
