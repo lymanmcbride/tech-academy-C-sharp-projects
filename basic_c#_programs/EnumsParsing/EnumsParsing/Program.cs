aa228550ef3795f8c521e0dad93542bf939a16ae
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-23 21:54:11 -0600

    Refactor to use Parse() and try/catch statement

--- basic_c#_programs/EnumsParsing/EnumsParsing/Program.cs
+++ basic_c#_programs/EnumsParsing/EnumsParsing/Program.cs
@@ -14,21 +14,35 @@ namespace EnumsParsing
             Console.WriteLine("Please enter the current day of the week");
             string TodayString = Console.ReadLine();
 
-
-            //cast to daysofweek
-            DaysOfWeek Today;//define variable that will eventually hold it
-            if (Enum.TryParse(TodayString, out Today) is false) //tryparse eliminates the need for exception handling. It returns false if it's not found
+            //USING PARSE()
+            try
             {
-                Console.WriteLine("Please enter a real day in english");
-                Main();
-            }
-            else
-            {
-                Enum.TryParse(TodayString, out Today);
+                DaysOfWeek Today = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), TodayString); //parses the enum and turns input into an enum variable
                 Console.WriteLine(Today);
                 Console.WriteLine((int)Today);
                 Console.ReadLine();
             }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Main();//recursive call to give the user another chance
+            }
+
+            //USING TRYPARSE
+            //cast to daysofweek
+            //DaysOfWeek Today;//define variable that will eventually hold it
+            //if (Enum.TryParse(TodayString, out Today) is false) //tryparse eliminates the need for exception handling. It returns false if it's not found
+            //{
+            //    Console.WriteLine("Please enter a real day in english");
+            //    Main();
+            //}
+            //else
+            //{
+            //    Enum.TryParse(TodayString, out Today);
+            //    Console.WriteLine(Today);
+            //    Console.WriteLine((int)Today);
+            //    Console.ReadLine();
+            //}
 
         }
         public enum DaysOfWeek
