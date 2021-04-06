d5955e152902a2f1b1444ce951099a1b68380568
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-04-05 22:17:14 -0600

    Create Database code first and add one student

---
+++ CodeFirstChallenge/CodeFirstConsoleApp/CodeFirstConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+
+namespace CodeFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                //create and save new Student
+                Console.Write("Student's First Name: ");
+                var firstName = Console.ReadLine();
+                Console.Write("Student's Last Name: ");
+                var lastName = Console.ReadLine();
+                Console.Write("Student's GPA: ");
+                decimal gpa = Convert.ToDecimal(Console.ReadLine());
+
+                Console.Write("Add an activity: ");
+                var activityName = Console.ReadLine();
+
+                var student = new Student { FirstName = firstName, LastName = lastName, GPA = gpa };
+                var activity = new Activity { ActivityName = activityName };
+                db.Students.Add(student);
+                db.Activity.Add(activity);
+                db.SaveChanges();
+
+                //query for viewing. needs system.LINQ
+                var query = from b in db.Students orderby b.LastName select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.LastName);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+            }
+        }
+    }
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Activity> Activity { get; set; }
+    }
+}
---
+++ CodeFirstChallenge/CodeFirstConsoleApp/CodeFirstConsoleApp/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace CodeFirstConsoleApp
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal GPA { get; set; }
+
+        public virtual List<Activity> Activity { get; set; }
+    }
+
+    public class Activity
+    {
+        public int ActivityId { get; set; }
+        public string ActivityName { get; set; }
+    }
+}
